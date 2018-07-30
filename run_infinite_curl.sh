23623ba7b595cae61aec62a492c62e3553c22acf
trabetti <talis@il.ibm.com>
2018-07-30 11:24:26 +0300

    some updates

--- run_infinite_curl.sh
+++ run_infinite_curl.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-IP=$1
-SERVICE=$2
+
+SERVICE=$1
+
+if [ $# -eq 2 ]
+  then
+    DOCKER_NAME=$2
+  else
+    DOCKER_NAME=envoyfrontproxyrandomservice_front-envoy_1
+fi
+
+IP=`docker inspect $DOCKER_NAME | grep IPAddress | tail -1 | sed 's/\"//g' | sed 's/,//' | awk '{print $2}'`
 
 if [ $# -eq 0 ]
   then
-    	echo "Usage: run_infinite_curl.sh IP_ADDRESS SERVICE_NAME"
+    	echo "Usage: run_infinite_curl.sh SERVICE_NAME [front-envoy's docker container name or id (default:envoyfrontproxyrandomservice_front-envoy_1]"
 else
    echo "command: curl -v $IP:80/service/$SERVICE"
    echo "Press [CTRL+C] to stop.."
    while :
       do
-	#curl -v 172.21.0.3:80/service/2
-	#curl -v $IP:80/service/$SERVICE
 	curl $IP:80/service/$SERVICE
 	#sleep 1
       done
