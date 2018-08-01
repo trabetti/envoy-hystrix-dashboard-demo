e4455cddfe2bc538e3e23aca8b3c479dc764dbda
talis <talis@il.ibm.com>
2018-08-01 11:28:37 +0300

    add option of printing all container's ip when no argument given to get_ip_by_container_name_or_id.sh

--- get_ip_by_container_name_or_id.sh
+++ get_ip_by_container_name_or_id.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-DOCKER_NAME=$1
-IP=`docker inspect $DOCKER_NAME | grep IPAddress | tail -1 | sed 's/\"//g' | sed 's/,//' | awk '{print $2}'`
-echo $DOCKER_NAME IP: $IP
+if [ $# -eq 1 ]
+   then	
+	DOCKER_NAME=$1
+	IP=`docker inspect $DOCKER_NAME | grep IPAddress | tail -1 | sed 's/\"//g' | sed 's/,//' | awk '{print $2}'`
+	echo $DOCKER_NAME IP: $IP
+  else
+        for i in $( docker ps -a | grep Up | awk '{print $NF}' ); do
+            	DOCKER_NAME=$i
+		IP=`docker inspect $DOCKER_NAME | grep IPAddress | tail -1 | sed 's/\"//g' | sed 's/,//' | awk '{print $2}'`
+		echo $DOCKER_NAME IP: $IP
+        done	
+	
+fi
